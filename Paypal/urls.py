a284771ea34a141f899d556a270f51c6382baf25
Hassan-YB <hassanbaras5@gmail.com>
2021-07-16 00:42:35 +0500

    Requirements

--- Paypal/urls.py
+++ Paypal/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ipn/',include('IPN.urls')),
+    path('',include('IPN.urls')),
     path('paypal/', include('paypal.standard.ipn.urls')),
 ]
